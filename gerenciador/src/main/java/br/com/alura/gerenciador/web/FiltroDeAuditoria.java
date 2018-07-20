a64676b9037e36452ac6bac178e6337891005dc7
Lucas de Oliveira Silva <Lucas@189.34.136.12>
2018-07-19 21:36:44 -0300

    Alterando para Cookie nunca expirar usando o Filtro pra isso

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = "/*")
 public class FiltroDeAuditoria implements Filter {
@@ -24,11 +25,12 @@ public class FiltroDeAuditoria implements Filter {
 			FilterChain chain) throws IOException, ServletException {
 
 		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
 		// String uri = req.getRequestURI();
 		// String usuario = getUsuario(req);
 		// Incurtando o código
-		System.out.println("Usuario: " + getUsuario(req) + " acessando a URI: "
-				+ req.getRequestURI());
+		System.out.println("Usuario: " + getUsuario(req, resp)
+				+ " acessando a URI: " + req.getRequestURI());
 
 		// depois de filtrar segue com o request e responde
 		chain.doFilter(request, response);
@@ -36,12 +38,14 @@ public class FiltroDeAuditoria implements Filter {
 	}
 
 	// METODO getUsuario
-	private String getUsuario(HttpServletRequest req) {
+	private String getUsuario(HttpServletRequest req, HttpServletResponse resp) {
 
 		Cookie cookie = new Cookies(req.getCookies()).buscaUsuarioLogado();
 		if (cookie == null) {
 			return "<deslogado>";
 		}
+		cookie.setMaxAge(10 * 60);
+		resp.addCookie(cookie);
 		return cookie.getValue();
 	}
 
